67bdce5dedad6528ecad1ac8464462b7dfe35d10
Ângelo Quintarelli Greenhalgh <angelogreenhah@hotmail.com>
2025-11-22 22:34:14 -0300

    :hammer: Implementação de aviso de áudio

--- src/Contexts/TaskContext/TaskContextProvider.tsx
+++ src/Contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
 import { TaskActionTypes } from './taskActions';
+import { loadBeep } from '../../utils/loadBeep';
 
 // Tipo do provedor do contexto
 type TaskContextProviderProps = {
@@ -14,34 +15,45 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   // Monitoramento do estado do aplicativo
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
-
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
   const worker = TimerWorkerManager.getInstance();
 
   worker.onmessage(e => {
     const countDownSeconds = e.data;
 
-    if(countDownSeconds <= 0){
+    if (countDownSeconds <= 0) {
+      if (playBeepRef.current) {
+        playBeepRef.current();
+        playBeepRef.current = null;
+      }
+
       dispatch({
-        type : TaskActionTypes.COMPLETE_TASK
+        type: TaskActionTypes.COMPLETE_TASK,
       });
       worker.terminate();
-    }else{
+    } else {
       dispatch({
-        type : TaskActionTypes.COUNT_DOWN,
-        payload: {secondsRemaining : countDownSeconds}
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
       });
     }
   });
 
   // Monitora o alteração de um valor de uma variável de estado
   useEffect(() => {
-    console.log(state);
-    if(!state.activeTask){
-      console.log("Worker terminado por falta de activeTask");
+    if (!state.activeTask) {
       worker.terminate();
     }
     worker.postMessage(state);
   }, [worker, state]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep();
+    } else {
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
