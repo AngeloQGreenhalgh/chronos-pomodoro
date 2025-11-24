2028f68a32a7b4f225c5f8bd699d8ac087b055b2
Ângelo Quintarelli Greenhalgh <angelogreenhah@hotmail.com>
2025-11-24 08:10:42 -0300

    :hammer: Ajuste para corrigir declaração de módulos

--- src/types/assets/assets.d.ts
+++ src/types/assets/assets.d.ts
@@ -2,3 +2,11 @@ declare module '*.mp3' {
   const src: string;
   export default src;
 }
+
+// src/vite-env.d.ts
+declare module '*.css';
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
